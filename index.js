55abef5eb5497dd2fb2e0abc0c040298d7a5bed4
Pernille1997 <152873806+Pernille1997@users.noreply.github.com>
2023-12-04 14:07:17 +0100

    Update index.js
    calender

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// Add event listeners to each day
+for (let i = 1; i <= 24; i++) {
+  const day = document.getElementById(`day${i}`);
+  const content = document.getElementById(`content${i}`);
+
+  day.addEventListener('click', () => {
+    // Hide all day contents
+    const allContents = document.querySelectorAll('.day-content');
+    allContents.forEach((el) => {
+      el.style.display = 'none';
+    });
+
+    // Display the content of the clicked day
+    content.style.display = 'block';
+  });
+}
 
